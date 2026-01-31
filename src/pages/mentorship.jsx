7a89e5a86bd51d0750a00618e8296ab9b7ba2e19
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-01-31 02:25:22 -0300

    feat: added mentorship  in router page

---
+++ src/pages/mentorship.jsx
@@ -0,0 +1,5 @@
+import { dataMentorship } from "../utils/data-mentorship";
+import Sections from "../components/sections/sections";
+export const Mentorship = () => (
+  <Sections titlePage="Mentoria" dataArray={dataMentorship} />
+);
