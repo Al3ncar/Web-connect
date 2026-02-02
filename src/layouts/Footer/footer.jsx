35dea2c2a646bf501509cc4af12fe4c27a4ba319
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-02-02 13:13:41 -0300

    feat: created footer section

---
+++ src/layouts/Footer/footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>© 2025 Vai na Web & Empower.</span> Todos os direitos reservados.
+        Projeto destinado exclusivamente a fins educativos.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
