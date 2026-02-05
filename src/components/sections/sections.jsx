f4729b97d4556260b308efc06f12413aa5505f88
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-02-05 00:07:53 -0300

    feat: added confirm and notification the in sections component

--- src/components/sections/sections.jsx
+++ src/components/sections/sections.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import Card from "../../components/card/card";
+import ConfirmModal from "../confirm/confirm";
+import Notification from "../notification/notification";
 
 const Sections = ({ titlePage, dataArray }) => {
+  const [getTitleCard, setTitleCard] = useState("");
+  const [openConfirm, setOpenConfirm] = useState(false);
+  const [openNotification, setOpenNotification] = useState(false);
+
   const onConfirm = (valid) => {
-    console.log(valid);
+    setOpenConfirm(true);
+    setTitleCard(valid);
   };
 
   return (
     <section className="sections">
       <h1>{titlePage || "Texto de section"}</h1>
+
+      <Notification
+        open={openNotification}
+        message="Inscrito com sucesso!"
+        duration={2000}
+        onClose={() => setOpenNotification(false)}
+      />
+
+      <ConfirmModal
+        open={openConfirm}
+        title={getTitleCard}
+        description="Tem certeza que deseja se inscrever nesse programa?!"
+        onCancel={() => setOpenConfirm(false)}
+        onConfirm={() => {
+          setOpenNotification(true);
+          setOpenConfirm(false);
+        }}
+      />
 
       <div className="sections__cards">
         {dataArray?.map((item) => {
@@ -24,7 +49,7 @@ const Sections = ({ titlePage, dataArray }) => {
             />
           );
         })}
-      </div> 
+      </div>
     </section>
   );
 };
