2adf7e338b05b0cf556b54afe58ab150b88e8b20
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-02-05 00:04:20 -0300

    feat: created home in page

---
+++ src/pages/home.jsx
@@ -0,0 +1,29 @@
+import AllHands from "../assets/banner/all-hands.png";
+import { Button } from "../components/button/btn";
+import { dataStatics } from "../utils/data/data-home";
+
+const HomePage = () => {
+  return (
+    <section className="home">
+      <section className="home--page">
+        <h1>Projetos Sociais que transformam </h1>
+        <p className="home--page--parg">
+          Conectamos sua empresa com projetos sociais impactantes. Juntos,
+          criamos mudanças reais na comunidade.
+        </p>
+        <Button type="square-secondary" label="Cadastrar Empresa" />
+        <div className="home--page--cards">
+          {dataStatics.map((item) => (
+            <aside key={item.id}>
+              <h2> {item.value}</h2>
+              <p>{item.description}</p>
+            </aside>
+          ))}
+        </div>
+      </section>
+      <img src={AllHands} alt="Uma mão com um mundinho pequeno no meio com varias pessoinhas de papel " />
+    </section>
+  );
+};
+
+export default HomePage;
