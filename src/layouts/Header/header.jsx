cee209aa0d30031193cd92511c324c243b070e28
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-02-05 00:08:54 -0300

    feat: added and used useClickOutSide in menu header

--- src/layouts/Header/header.jsx
+++ src/layouts/Header/header.jsx
@@ -1,18 +1,52 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { navData, menuDropdown } from "../../utils/data-nav";
-
+import { useEffect, useState, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { navData } from "../../utils/data/data-nav";
+import { useClickOutside } from "../../utils/func/useClickOutSide";
 import Ilogo from "../../assets/logo/icon.png";
 
-const Header = () => {
+const Header = ({ imgProfile, userName }) => {
   const [isOpenMenu, setOpenMenu] = useState(false);
+  const [visibleItemMenu, setVisibleItemMenu] = useState(0);
+  
+  const menuRef = useRef(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    function handleResize() {
+      const width = window.innerWidth;
+      const screenMenu = [
+        { min: 1224, action: () => setVisibleItemMenu(0) },
+        { min: 1024, action: () => setVisibleItemMenu(1) },
+        { min: 824, action: () => setVisibleItemMenu(2) },
+        { min: 724, action: () => setVisibleItemMenu(3) },
+        { min: 624, action: () => setVisibleItemMenu(4) },
+        { min: 524, action: () => setVisibleItemMenu(5) },
+      ];
+      const responsiveScreen = screenMenu.find((item) => width >= item.min);
+      responsiveScreen?.action();
+      setOpenMenu(false);
+    }
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const closeMenu = () => setOpenMenu(false);
+
+  const backMenu = () => {
+    closeMenu();
+    navigate("/");
+    window.location.reload();
+  };
+  useClickOutside(menuRef, closeMenu);
 
   return (
     <header className="header-container">
       <img src={Ilogo} alt="Icone de mão segurando um coração de desenho" />
       <nav>
         <ul>
-          {navData.map((item) => (
+          {navData.slice(visibleItemMenu).map((item) => (
             <Link to={item.toLocal} key={item.id}>
               <li>{item.title}</li>
             </Link>
@@ -23,19 +57,26 @@ const Header = () => {
       <div className="header-container--profile">
         <button onClick={() => setOpenMenu((state) => !state)}>
           <img
-            src="https://avatars.githubusercontent.com/u/93939408?v=4"
-            alt="Icone de perfil do Igor Alencar"
+            id="profile"
+            src={imgProfile}
+            alt={`Imagem de perfil ${userName}`}
           />
         </button>
       </div>
 
       {isOpenMenu && (
-        <ul className="header-container--profile__menu"> 
-          {menuDropdown.map((item) => (
-            <Link to={item.toLocal} key={item.id}>
+        <ul ref={menuRef} className="header-container--profile__menu">
+          <Link to="/about" onClick={() => closeMenu()}>
+            <li>{userName}</li>
+          </Link>
+          {navData.slice(0, visibleItemMenu).map((item) => (
+            <Link to={item.toLocal} key={item.id} onClick={() => closeMenu()}>
               <li>{item.title}</li>
             </Link>
           ))}
+          <Link to="/" onClick={() => backMenu()}>
+            <li>Sair</li>
+          </Link>
         </ul>
       )}
     </header>
